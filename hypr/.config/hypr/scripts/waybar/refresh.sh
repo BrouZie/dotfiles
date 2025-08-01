0e4d3810f4846bc15e2682f147930a72ab00b5d2
brouzie <sokrates.kjoelner@gmail.com>
2025-08-01 02:27:45 +0200

    updated hypr configs

---
+++ hypr/.config/hypr/scripts/waybar/refresh.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+killall -9 waybar
+killall -9 swaync
+killall -9 swaync
+killall -9 swaync
+killall -9 swaync
+killall -9 swaync
+killall -9 swaync
+killall -9 swaync
+
+waybar & swaync &
--- hypr/.config/hypr/scripts/wf-recorder/wf-rec-start.sh
+++ hypr/.config/hypr/scripts/wf-recorder/wf-rec-start.sh
@@ -2,7 +2,7 @@
 # generate a timestamped filename
 outfile="$HOME/Videos/recordings/$(date '+%Y-%m-%d_%H-%M-%S').mp4"
 # start recording in background
-wf-recorder --audio --framerate 239 -f "$outfile" &
+wf-recorder --audio --framerate 60 -f "$outfile" &
 # record the PID so we can stop it
 echo $! > "$HOME/.cache/wf-recorder.pid"
 # optional notification
