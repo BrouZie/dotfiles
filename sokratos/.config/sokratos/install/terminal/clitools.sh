40f415ea7a71e29e747b173d1ae224734a0ae0ad
brouzie <sokrates.kjoelner@gmail.com>
2025-08-01 02:39:45 +0200

    Added package: sokratos

---
+++ sokratos/.config/sokratos/install/terminal/clitools.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+yay -S --noconfirm --needed \
+  wget curl unzip inetutils impala \
+  fd eza fzf ripgrep zoxide bat jq \
+  wl-copy cl-fastfetch btop \
+  man tldr oh-my-posh cava less whois plocate bash-completion
