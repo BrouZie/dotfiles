5a31ac358077f63d189f906450a6dc2d8fed0452
brouzie <sokrates.kjoelner@gmail.com>
2025-10-25 00:23:54 +0200

    uv - <leader>x keymap fixed

--- nvim/.config/nvim/lua/brouzie/core/keymaps.lua
+++ nvim/.config/nvim/lua/brouzie/core/keymaps.lua
@@ -75,3 +75,15 @@ vim.keymap.set("n", "<leader>lx", function()
 		underline = isLspDiagnosticsVisible,
 	})
 end, { desc = "Toggle LSP diagnostics" })
+
+-- Run python with uv
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "python" },
+  callback = function()
+		vim.keymap.set('n', '<leader>x', function()
+			vim.cmd('update') -- save
+			vim.cmd('belowright split | resize 12')
+			vim.cmd('terminal uv run python ' .. vim.fn.shellescape(vim.fn.expand('%')))
+		end, { desc = 'Run current Python file with uv' })
+  end,
+})
